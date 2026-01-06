095ff006ab3ae0fd64aecd9b1fe0766f234c0e4c
CoolCat467 <52022020+CoolCat467@users.noreply.github.com>
2026-01-06 00:47:19 -0600

    Use 3.14 in CI and update pre-commit hooks

--- check.sh
+++ check.sh
@@ -81,6 +81,8 @@ if git status --porcelain | grep -q "uv.lock"; then
     echo "::endgroup::"
 fi
 
+codespell || EXIT_STATUS=$?
+
 # Finally, leave a really clear warning of any issues and exit
 if [ $EXIT_STATUS -ne 0 ]; then
     cat <<EOF
